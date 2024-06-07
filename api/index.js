b7177d526b7e6d9fd0dd24fac79ba5db0cb43be5
Amratansh Shrivastava <ashri1205@gmail.com>
2024-06-07 10:50:48 +0530

    fixing cors access

--- api/index.js
+++ api/index.js
@@ -12,16 +12,10 @@ const orderRoutes = require("../routes/order.routes.js");
 
 const app = express();
 
+app.use(cors({
+  origin: "https://digital-menu-sandy.vercel.app/auth",
+}));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
-
-app.use(cors());
 
 connectDb();
 
