213fbc0e5e72a5757ab675fa1b8d5ef90c9f7663
Amratansh Shrivastava <ashri1205@gmail.com>
2024-06-07 10:44:35 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,10 +12,16 @@ const orderRoutes = require("../routes/order.routes.js");
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://digital-menu-sandy.vercel.app',
-}));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
+app.use(cors());
 
 connectDb();
 
